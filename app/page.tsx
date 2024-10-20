e87cc7f2291f40e5705e2ab8a66c244f0bce6ab5
Petr Kroutil <pkroutil1@seznam.cz>
2024-10-20 19:32:04 +0200

    main-nav, dark/light mode, container

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Container from '@/components/container';
 import LatestPost from '@/components/home/latest-posts';
+import { MainNav } from '@/components/main-nav';
 
 export default function Home() {
   return (
-    <>
-      <LatestPost />
-    </>
+    <Container>
+      <MainNav />
+      <main>
+        <div>
+          <LatestPost />
+        </div>
+      </main>
+    </Container>
   );
 }
